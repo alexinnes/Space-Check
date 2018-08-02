75e3326af49963607628578567eddbe9035dddc2
Alex Innes <ainnes@rmplc.net>
2018-08-02 16:28:06 +0100

    Initial commit

---
+++ SpaceCheck.Tests.ps1
@@ -0,0 +1,14 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "SpaceCheck" {
+    It "Is the FreeSpace the correct type" {
+        $driveSpace = get-drivespace
+        $driveSpace.FreeSpaceGB | Should beoftype system.Double
+    }
+    It "Is the TotalSize the correct type" {
+        $driveSpace = get-drivespace
+        $driveSpace.totalsizeGB | Should beoftype system.Double
+    }
+}
---
+++ SpaceCheck.ps1
@@ -0,0 +1,50 @@
+<#
+.Synopsis
+   Gets disk space of computers specified
+.DESCRIPTION
+   Uses WMI to get disk size and available space on computers specified. Will look at the disks you ask
+.EXAMPLE
+   get-DriveSpace -driveLetter C -Computer SomeStation
+.EXAMPLE
+    Get-DriveSpace -Computer Station1,Station2,Station3
+.EXAMPLE
+#>
+
+function Get-DriveSpace {
+
+[CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false, Position=1)]
+            #Default Param is the local computer.
+            [string[]]$computer = $ENV:ComputerName,
+
+            [Parameter(Mandatory=$false, Position=0)]
+            [string]$driveLetter = "C"
+    )
+    $outputObj = @()
+    $SpaceObj = @()
+    $wmiError
+    foreach($c in $computer){
+        if(Test-Connection $c -Count 1 -Quiet){
+            $space = Get-WmiObject win32_Logicaldisk -ComputerName $c -filter "DeviceID='$driveLetter`:'" -ErrorAction SilentlyContinue
+            $spaceObj = [ordered]@{
+                ComputerName = $c
+                DriveLetter = $driveLetter
+                FreespaceGB = [math]::round($space.freespace /1GB,2)
+                TotalSizeGB = [math]::round($space.size /1GB,2)
+            }#end spaceObj
+
+            $outputObj += $spaceObj
+        }else{
+            $spaceObj = [ordered]@{
+                ComputerName = $c
+                DriveLetter = $driveLetter
+                FreespaceGB = "Offline"
+                TotalSizeGB = "Offline"
+            }#end SpaceObj
+
+            $outputObj += $spaceObj
+        }
+    }
+    $outputObj
+}
